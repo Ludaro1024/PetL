75643239f620c2c58e1fe6d9e88ed8ac81890c0b
ludaro <92580232+waschmaschvanlu@users.noreply.github.com>
2022-04-26 15:19:17 +0200

    Create PetL.sql

---
+++ PetL.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `pet` tinyint(1) DEFAULT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `users` (
+  `pet` tinyint(1) DEFAULT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
