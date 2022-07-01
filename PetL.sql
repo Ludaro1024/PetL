de531e7225e05155cdf01ed995f1dd35c806e443
ludaro <92580232+waschmaschvanlu@users.noreply.github.com>
2022-07-01 21:50:52 +0200

    Update PetL.sql

--- PetL.sql
+++ PetL.sql
@@ -3,8 +3,8 @@ ALTER TABLE `users` ADD COLUMN IF NOT EXISTS `pet` TINYINT;
 CREATE TABLE IF NOT EXISTS `pet` (
   `identifier` varchar(60) NOT NULL,
   `petmodel` varchar(100) NOT NULL,
-  `food` int(11) NOT NULL, DEFAULT `0`, INT CHECK (`food` BETWEEN 0 and 100));
-  `thirst` int(11) NOT NULL, DEFAULT `0`,   INT CHECK (`food` BETWEEN 0 and 100));
+   `food` INT(11) DEFAULT `0`, 
+   `thirst` INT(11) DEFAULT `0`,
    `nickname`varchar(100),
   PRIMARY KEY (`identifer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
